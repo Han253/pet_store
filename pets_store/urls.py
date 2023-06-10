240b612f804797d6ecad5298792bb7d3bb07989e
han253 <han315730@gmail.com>
2023-06-10 16:54:19 -0500

    add login

--- pets_store/urls.py
+++ pets_store/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('commons.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('productos/',include('productos.urls')),
     path('admin/', admin.site.urls),
 ]
